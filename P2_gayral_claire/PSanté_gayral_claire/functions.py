ee379ea39f878364db874aa25db94fb153bf161f
Claire Gayral <c.gayral@wanadoo.fr>
2021-04-21 11:10:18 +0200

    ind plot with coloration + use dict of color mapping for the corr circle

--- P2_gayral_claire/PSanté_gayral_claire/functions.py
+++ P2_gayral_claire/PSanté_gayral_claire/functions.py
@@ -20,16 +20,17 @@ def get_str_vars(list_of_var):
             len_line = 0
     return(res)
 
-def draw_cluster_legend(ax2,cluster,my_color_set):
+def draw_cluster_legend(ax2,clustering, corresp_color_dict):
     ## plot the legend with colored arrow
     # number of clusters : 
-    K = max(cluster)+1
+    K = len(clustering.values.categories)
+    my_color = clustering.values.categories.map(corresp_color_dict)
     # plot parallel arrows :
     ax2.quiver(np.zeros(K),np.arange(0,K),np.ones(K),np.zeros(K),
-               color = my_color_set[:K])
+               color = my_color)
     # plot legend text next to the respective arrow :
-    for k in range(K):
-        cluster_var = get_str_vars(list(cluster[cluster == k].index.values))
+    for k in clustering.values.categories :
+        cluster_var = get_str_vars(list(clustering[clustering == k].index.values))
         ax2.text(0.2, k , str(cluster_var), fontsize='11',
                  ha='left', va='center' , alpha=1)
     # set limits : 
@@ -41,19 +42,38 @@ def draw_cluster_legend(ax2,cluster,my_color_set):
     ax2.get_yaxis().set_visible(False)
     plt.axis("off")
     return(ax2)
-# fig2, ax2 = plt.subplots(1,1)
-# draw_cluster_legend(ax2, cluster)
-# plt.show()
 
-def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None, cluster = None):
+def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None, clustering = None):
+    ## set coloration palette : 
+    if clustering is not None : 
+        my_color_set = ['#154406', '#15b01a', '#fffd01', '#f97306', '#c0022f',
+                '#0343df', '#fe02a2', '#8b3103', '#7e1e9c', '#017371',
+                '#380282', '#6b8ba4', '#75bbfd', '#ff81c0', '#c79fef',
+                '#ff073a', '#fdaa48', '#fea993', '#fe7b7c', '#c20078',
+                '#029386', '#677a04', '#b25f03', '#070d0d']
+        corresp_color_dict = dict(zip(clustering.values.categories, my_color_set))
+        my_color = clustering.values.map(corresp_color_dict)
+
+    else : 
+        my_color = "grey"
+    ## set global plot arguments : 
+    plot_kwargs = {"alpha":1, "color":my_color}
     ## draw the correlation circle for pca : 
     for d1, d2 in axis_ranks: # On affiche les 3 premiers plans factoriels, donc les 6 premières composantes
         if d2 < n_comp:
-            fig = plt.figure(figsize = (18,6))
-            # initialisation de la figure
-            ax1 = fig.add_subplot(1,2,1)
+            ## initialise figure
+            if clustering is not None : 
+                fig = plt.figure(figsize = (18,6))
+                ## affichage de la legende du clustering en couleur : 
+                ax2 = fig.add_subplot(1,2,2)
+                draw_cluster_legend(ax2, clustering, corresp_color_dict)
+                # initialisation de la figure "cercle"
+                ax1 = fig.add_subplot(1,2,1)
+            else : 
+                fig = plt.figure(figsize = (9,6))
+                ax1 = fig.add_subplot(1,1,1)
 
-            # détermination des limites du graphique
+            ## détermination des limites du graphique
             if lims is not None :
                 xmin, xmax, ymin, ymax = lims
             elif pcs.shape[1] < 30 :
@@ -61,90 +81,81 @@ def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0,
             else :
                 xmin, xmax, ymin, ymax = min(pcs[d1,:]), max(pcs[d1,:]), min(pcs[d2,:]), max(pcs[d2,:])
 
-            # affichage des flèches
-            # gestion de la couleur si cluster : 
-            if cluster is not None : 
-                my_color_set = ['#c0022f', '#ff073a', '#f97306', '#fdaa48', 
-                        '#fffd01', '#fea993', '#fe7b7c', '#ff81c0', '#fe02a2', 
-                        '#c20078', '#c79fef', '#7e1e9c', '#380282', '#0343df', 
-                        '#75bbfd', '#6b8ba4', '#029386', '#017371', '#15b01a', 
-                        '#677a04', '#b25f03', '#8b3103', '#070d0d', '#c5c9c7']
-                my_color = [my_color_set[i] for i in cluster.values]
-            else : 
-                my_color = "grey"
-
-
-            # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
+            ## affichage des fleches :
             if pcs.shape[1] < 30 :
-                ax1.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
-                           pcs[d1,:], pcs[d2,:], angles='xy', scale_units='xy', 
-                           scale=1, color=my_color)
+                ax1.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]), ## depart points
+                           pcs[d1,:], pcs[d2,:], ## movement in each direction
+                           angles='xy', scale_units='xy',**plot_kwargs)
                 # (voir la doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
-            else:
+            else: # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
                 lines = [[[0,0],[x,y]] for x,y in pcs[[d1,d2]].T]
                 ax1.add_collection(LineCollection(lines, axes=ax, alpha=.1, color=my_color))
 
-            # affichage des noms des variables  
+            ## affichage des noms des variables  
             if labels is not None:  
                 for i,(x, y) in enumerate(pcs[[d1,d2]].T):
                     if x >= xmin and x <= xmax and y >= ymin and y <= ymax :
                         ax1.text(x, y, labels[i], fontsize='14', ha='center', 
                                  va='center', rotation=label_rotation, color="blue", alpha=0.5)
 
-            # affichage du cercle
+            ## affichage du cercle
             circle = plt.Circle((0,0), 1, facecolor='none', edgecolor='b')
             plt.gca().add_artist(circle)
 
-            # définition des limites du graphique
+            ## définition des limites du graphique
             ax1.set_xlim(xmin, xmax)
             ax1.set_ylim(ymin, ymax)
 
-            # affichage des lignes horizontales et verticales
+            ## affichage des lignes horizontales et verticales
             plt.plot([-1, 1], [0, 0], color='grey', ls='--')
             plt.plot([0, 0], [-1, 1], color='grey', ls='--')
 
-            # nom des axes, avec le pourcentage d'inertie expliqué
+            ## nom des axes, avec le pourcentage d'inertie expliqué
             ax1.set_xlabel('F{} ({}%)'.format(d1+1, 
                                 round(100*pca.explained_variance_ratio_[d1],1)))
             ax1.set_ylabel('F{} ({}%)'.format(d2+1, 
                                 round(100*pca.explained_variance_ratio_[d2],1)))
             ax1.set_title("Cercle des corrélations (F{} et F{})".format(d1+1, d2+1))
-
-            ## affichage de la legende du clustering en couleur : 
-            if cluster is not None : 
-                ax2 = fig.add_subplot(1,2,2)
-                draw_cluster_legend(ax2, cluster,my_color_set)
-            plt.show(block=False)
+    plt.show(block=False)
             
         
-def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
+def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, ind_labels=None, alpha=1, clustering = None):
+    # args are as defined just above 
+    plot_kwargs = {"marker":"x", "alpha":alpha, 's':20}#, "label" : clustering.values.categories}
+    # set dict of color if clustering : 
+    if clustering is not None : 
+        my_color_set = ['#154406', '#15b01a', '#fffd01', '#f97306', '#c0022f', '#0343df', '#fe02a2', '#8b3103', 
+                    '#7e1e9c', '#017371', '#380282', '#6b8ba4', '#75bbfd', '#ff81c0', '#c79fef', '#ff073a', 
+                    '#fdaa48', '#fea993', '#fe7b7c', '#c20078', '#029386', '#677a04', '#b25f03', '#070d0d']
+        corresp_color_dict = dict(zip(clustering.values.categories, my_color_set))
+
     for d1,d2 in axis_ranks:
         if d2 < n_comp:
- 
+            ax1 = "Axis"+ str(d1+1)
+            ax2 = "Axis"+ str(d2+1)
             # initialisation de la figure       
-            fig = plt.figure(figsize=(7,6))
-        
-            # affichage des points
-            if illustrative_var is None:
-                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
-            else:
-                illustrative_var = np.array(illustrative_var)
-                for value in np.unique(illustrative_var):
-                    selected = np.where(illustrative_var == value)
-                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
-                plt.legend()
+            fig = plt.figure(figsize=(12,10))
+            if clustering is not None :
+                for k in clustering.values.categories:
+                    cluster_index = clustering[clustering==k].index
+    #                 print(X_projected.loc[cluster_index, ax1])
+                    plt.scatter(X_projected.loc[cluster_index, ax1],X_projected.loc[cluster_index, ax2], 
+                                color=corresp_color_dict[k], label = k, **plot_kwargs)
+                    plt.legend()
 
+            else : 
+                plt.scatter(X_projected[ax1], X_projected[ax2], **plot_kwargs)
             # affichage des labels des points
-            if labels is not None:
+            if ind_labels is not None:
                 for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
-                    plt.text(x, y, labels[i],
+                    plt.text(x, y, ind_labels[i],
                               fontsize='14', ha='center',va='center') 
-                
+
             # détermination des limites du graphique
-            boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
+            boundary = np.max(np.abs(X_projected.values[:, [d1,d2]])) * 1.1
             plt.xlim([-boundary,boundary])
             plt.ylim([-boundary,boundary])
-        
+
             # affichage des lignes horizontales et verticales
             plt.plot([-100, 100], [0, 0], color='grey', ls='--')
             plt.plot([0, 0], [-100, 100], color='grey', ls='--')
